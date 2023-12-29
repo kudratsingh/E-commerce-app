76923d6b220094dae99b78cbf72c1662cfd38b06
Kudrat Singh <singhkudrat1999@gmail.com>
2023-12-29 00:59:21 -0800

    updated checkout modal

--- frontend/src/components/cart-components/SubtotalBox.js
+++ frontend/src/components/cart-components/SubtotalBox.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import './SubtotalBox.css';
+import React, { useState } from "react";
+import "./SubtotalBox.css";
 
-const SubtotalBox = ({ totalCost }) => {
-    return (
-        <div className="subtotal-box">
-            <h4>Subtotal: ${totalCost.toFixed(2)}</h4>
-            <button className="checkout-button">Proceed to Checkout</button>
-        </div>
-    );
-}
+const SubtotalBox = ({ totalCost, onProceedToCheckout }) => {
+
+  return (
+    <div className="subtotal-box">
+      <h4>Subtotal: ${totalCost.toFixed(2)}</h4>
+      <button
+        onClick={onProceedToCheckout}
+        className="checkout-button"
+      >
+        Proceed to Checkout
+      </button>
+    </div>
+  );
+};
 
 export default SubtotalBox;
